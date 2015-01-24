081481213b7c45d1b9b88c46bb19eb8743e659a3
Mostafa Gamal <Mctifa@gmail.com>
2015-01-24 19:33:46 +0200

    door

---
+++ Assets/texturechanger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class texturechanger : MonoBehaviour {
+	public GameObject x;
+	public Texture y;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag=="Player")
+			x.renderer.material.mainTexture= y;
+	}
+
+}
