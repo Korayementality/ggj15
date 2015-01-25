d3053b5cde4a3958c5043fcefbdfcf1e74b921c5
Mostafa Gamal <Mctifa@gmail.com>
2015-01-25 05:23:29 +0200

    astro, radio

---
+++ Assets/Astronaut/RotateAstronaut.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var tumble : float;
+var object : Transform;
+var speed : int;
+
+function Start () : void {
+    rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+
+}
+
+
+function Update() {
+   transform.RotateAround (Vector3.zero, Vector3.up, speed * Time.deltaTime);
+}
+
