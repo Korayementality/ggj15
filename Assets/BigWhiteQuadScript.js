33a6a07bde6ea80a18c42d8ba99b28d4a9e0ddc7
Korayementality <ali.korayem@yahoo.com>
2015-01-25 15:34:25 +0200

    Final

---
+++ Assets/BigWhiteQuadScript.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+
+var character : GameObject;
+var EndingTexture : GameObject;
+
+function Start () {
+
+	character = GameObject.Find("/Character");
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(col : Collider) {
+
+	if(col.gameObject.tag == "Player")
+	{
+		col.gameObject.GetComponent(CharaterController).enabled = false;
+		col.gameObject.GetComponent(MouseOrbit).enabled = false;
+		InvokeRepeating("MoveInZ", 0, 0.5);
+		InvokeRepeating("RotateInY", 2, 0.5);
+		col.gameObject.rigidbody.drag = 0;
+		col.gameObject.rigidbody.angularDrag = 0;
+		col.gameObject.rigidbody.constraints = RigidbodyConstraints.None;
+		col.gameObject.rigidbody.useGravity = false;
+		Invoke("Blackout", 10);
+	}
+
+}
+
+function MoveInZ() {
+
+	character.rigidbody.velocity.z += 1;
+
+}
+
+function RotateInY() {
+
+	character.rigidbody.angularVelocity.y *= -5*character.rigidbody.angularVelocity.y+5;
+
+}
+
+function Blackout() {
+
+	RenderSettings.ambientlight = Color.black;
+
+}
+
+function ShowEndingTexture() {
+
+	EndingTexture.SetActive(true);
+
+}
---
+++ Assets/QuadQuote.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+var sequenceStarted : boolean;
+var hasRotated : boolean;
+var textureCube : GameObject;
+var destroyedQuad : GameObject;
+var tree : GameObject;
+
+function Start () {
+
+	sequenceStarted = false;
+	hasRotated = false;
+}
+
+function Update () {
+
+	if(GameObject.Find("/Character").transform.eulerAngles.y >= 180 && sequenceStarted && !hasRotated)
+	{
+		MoveTree();
+		hasRotated = true;
+		Destroy(tree, 2);
+	}
+
+}
+
+function OnTriggerEnter (col : Collider) {
+
+	if(col.gameObject.tag == "Player" && !sequenceStarted){
+		textureCube.SetActive(true);
+		Destroy(destroyedQuad, 2);
+		sequenceStarted = true;
+	}
+}	
+
+
+function MoveTree() {
+
+	tree.transform.position = Vector3(tree.transform.position.x, tree.transform.position.y, tree.transform.position.z - 6);
+
+}
